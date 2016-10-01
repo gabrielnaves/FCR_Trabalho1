007c4a940864950a4a5be534b478eeb20f41eca7
Gabriel Naves da Silva <gabri.navess@gmail.com>
2016-10-01 16:12:57 -0300

    Added a rectangle collider to the scenario.
    This way we won't risk losing the robot's position over time.

---
+++ Assets/Scripts/RectangleCollider.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RectangleCollider : MonoBehaviour {
+
+	public float width;
+	public float height;
+
+	private EdgeCollider2D edgeCollider;
+
+	void Start() {
+		edgeCollider = GetComponent<EdgeCollider2D>();
+		if (edgeCollider == null)
+			AddColliderToGameObject();
+		GenerateCollider();
+	}
+
+	// Generating on update is useful for editing collider by hand, this method should not execute during play mode.
+#if UNITY_EDITOR
+	void Update() {
+		if (!Application.isPlaying) {
+			if (edgeCollider == null)
+				AddColliderToGameObject();
+			GenerateCollider();
+		}
+	}
+#endif
+
+	private void AddColliderToGameObject() {
+		edgeCollider = gameObject.AddComponent<EdgeCollider2D>();
+	}
+
+	private void GenerateCollider() {
+		Vector2[] points = new Vector2[5];
+		points[0] = new Vector2(-width/2, -height/2);
+		points[1] = new Vector2(width/2, -height/2);
+		points[2] = new Vector2(width/2, height/2);
+		points[3] = new Vector2(-width/2, height/2);
+		points[4] = new Vector2(-width/2, -height/2);
+		edgeCollider.points = points;
+	}
+}
