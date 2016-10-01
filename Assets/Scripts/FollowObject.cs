611e04c74334a948ffb57812597fdcfc5a240ff0
Gabriel Naves da Silva <gabri.navess@gmail.com>
2016-10-01 16:04:52 -0300

    Camera now follows the robot

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary> Makes an object follow a given object at a given offset value. </summary>
+public class FollowObject : MonoBehaviour
+{
+    public GameObject followed;
+    public Vector3 offset;
+
+    private Vector3 followedPos;
+
+    void Start() {
+        UpdatePosition();
+    }
+
+    void Update() {
+        UpdatePosition();
+    }
+
+    void UpdatePosition() {
+        if (followed != null) {
+            followedPos = followed.transform.position;
+            transform.position = followedPos + offset;
+        }
+    }
+}
